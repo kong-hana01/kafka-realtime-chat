358bfdd6340d0a76d40208f6bb9a08de966de13e
최한빈 <gksqlsl11@khu.ac.kr>
2022-06-09 01:24:22 +0900

    Change api address

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,7 @@
-const API_URL = process.env.API_URL || "http://localhost:5050";
-const CHAT_API_URL = process.env.CHAT_API_URL || "ws://localhost:3001";
+// const API_URL = process.env.API_URL || "http://localhost:5050";
+// const CHAT_API_URL = process.env.CHAT_API_URL || "ws://localhost:3001";
+
+const API_URL = "https://franz.sinwoobang.me";
+const CHAT_API_URL = "wss://franz.sinwoobang.me/chat";
 
 export { API_URL, CHAT_API_URL };
