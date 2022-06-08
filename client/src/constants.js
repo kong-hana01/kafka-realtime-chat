8592cf0af14df33440fa1ccf9ad4eb629cf52717
Sin-Woo Bang <sinwoobang@gmail.com>
2022-06-09 03:31:56 +0900

    Change default port of ws

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,4 @@
 const API_URL = process.env.API_URL || "http://localhost:5050";
-const CHAT_API_URL = process.env.CHAT_API_URL || "ws://localhost:3001";
+const CHAT_API_URL = process.env.CHAT_API_URL || "ws://localhost:5051";
 
 export { API_URL, CHAT_API_URL };
