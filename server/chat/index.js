6b3db7122f2f11e697592f4a2e3d779122fc07b8
Sin-Woo Bang <sinwoobang@gmail.com>
2022-06-08 20:53:22 +0900

    Implement rooms

--- server/chat/index.js
+++ server/chat/index.js
@@ -1,6 +1,5 @@
 import { Kafka, logLevel } from "kafkajs";
 import { generateUniqueID } from "../utils.js";
-import { getRoomId } from "./room.js";
 
 const kafka = new Kafka({
   clientId: "app",
@@ -50,40 +49,4 @@ const getMsgHistory = async (roomId, cb) => {
   });
 };
 
-const receiveMsg = async (userId, roomId) => {
-  return null;
-};
-
-const createRoom = async (senderId, receiverId) => {
-  const roomId = getRoomId(senderId, receiverId);
-
-  const producer = kafka.producer();
-  await producer.connect();
-
-  const admin = kafka.admin();
-  admin.connect();
-
-  await admin.createTopics({
-    waitForLeaders: true,
-    topics: [{ topic: roomId }],
-  });
-
-  await producer.disconnect();
-  await admin.disconnect();
-
-  return roomId;
-};
-
-const deleteRoom = async (roomId) => {
-  const admin = kafka.admin();
-  await admin.connect();
-
-  await admin.deleteTopics({
-    topics: [roomId],
-    timeout: 3000,
-  });
-
-  await admin.disconnect();
-};
-
-export { sendMsg, getMsgHistory, receiveMsg, createRoom, deleteRoom };
+export { sendMsg, getMsgHistory };
